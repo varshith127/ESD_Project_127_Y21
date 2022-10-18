8c2350e129335f99fd477199e35f9af41065c5b3
varshith127 <110653034+varshith127@users.noreply.github.com>
2022-10-18 09:35:21 +0530

    Update Room lighting.c

--- Room lighting.c
+++ Room lighting.c
@@ -3,14 +3,13 @@ sbit ir=P2^4; //ir sensor is connected to pin 4 of port 2
 sbit led=P2^1; //led is connected to pin 1 of port 2
 void main()
 {
+	int i;
 	while(1)
 	{
 		if(ir==0) //pir sensor senses the presence of human
-		{
-				if(led==0)
-					led=1; //led is turned on
-				else
-					led=0; //led is turned off
-		}
+			led=1;
+		else
+			led=0;
+		for(i=0;i<20000;i++)
 	}
 }
